be51e1f4adc9bef84283371db78b6c0cd0edf403
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2026-05-19 07:58:39 +0200

    Bump versionName to 0.2 / versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "no.fyhn.uvindex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
         buildConfigField("String", "GIT_DESCRIBE", "\"$gitDescribe\"")
     }
 
